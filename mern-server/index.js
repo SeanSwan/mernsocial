e7f4a4845c7ddea37be9a7b17c71fec8f6457c8a
Sean Swan <ogpswan@YAHOO.COM>
2018-12-07 23:06:39 -0800

    dealing vith dpreciation errors focusing on storing PW properly

--- mern-server/index.js
+++ mern-server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
+const authRoutes = require("./routes/auth");
 
 
 const PORT = 8081;
--- mern-server/models/index.js
+++ mern-server/models/index.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb:/localhost/mernsocial" , {
-  KeepAlive: true,
-  useMongoClient: true
+  useNewUrlParser: true
+  //useMongoClient: true // recived error saying this is not used in mongo 5x
 });
