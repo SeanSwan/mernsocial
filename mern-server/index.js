845fc3fb437f6b0110807a5b2f614a6719b02f2a
Sean Swan <ogpswan@YAHOO.COM>
2018-12-07 22:00:56 -0800

    added models/user.js schema  as well as index.js and message.js file

--- mern-server/index.js
+++ mern-server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
+
 
 const PORT = 8081;
 
@@ -17,6 +19,10 @@ app.use(function (req, res, next) {
   err.status = 404;
   next(err);
 });
+
+
+app.use(errorHandler);
+
 
 app.listen(PORT, function() {
   console.log(`Server is starting on port ${PORT}`);
