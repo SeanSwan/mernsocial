cd620d25358ffa05161e6a606b3e0159f2426989
Sean Swan <ogpswan@YAHOO.COM>
2018-12-12 23:44:56 -0800

    Working on promises error

--- mern-server/models/index.js
+++ mern-server/models/index.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb:/localhost/mernsocial" , {
+mongoose.connect("mongodb:/localhost:8081/mern-social" , {
   useNewUrlParser: true
   //useMongoClient: true // recived error saying this is not used in mongo 5x
 });
 
 module.exports.User = require("./user");
-module.exports.Messages = require("./messages");
+module.exports.Messages = require("./message");
